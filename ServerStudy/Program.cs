ebbca29f6165da0321f4d1c74009b6b9be95765a
LucaSeoyoun <zzzzqqqw@naver.com>
2024-08-15 16:38:20 +0900

    ReaderWriterLock
    - GetRewardById(n)
    - .Enter/ExitReadLock()
    - .Enter/ExitWriteLock()

--- ServerStudy/Program.cs
+++ ServerStudy/Program.cs
@@ -6,64 +6,34 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class Lock
-    {
-        AutoResetEvent _available = new AutoResetEvent(true);
-        // AutoResetEvent는 문이 열려 점유한 뒤 문이 자동으로 닫히도록 되어 있음.
-        //ManualResetEvent _available = new ManualResetEvent(true);
-
-        public void Acquire()
-        {
-            _available.WaitOne(); // 입장 시도
-            //_available.Reset(); // Reset()은 문을 닫는 작업으로써 이미 AutoResetEvent의 작업과 중복됨.
-        }
-
-        public void Release()
-        {
-            _available.Set();
-        }
-    }
-
-
     class Program
     {
-        static Lock _locked = new Lock();
-        static int _num = 0;
-        //static Mutex _lock = new Mutex();
-        // Mutex는 커널에서 지원해주는 것이기에 속도가 굉장히 느림.
+        static ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
 
-        static void Thread_1()
+        class Reward
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                _locked.Acquire();
-                //_lock.WaitOne();
-                _num++;
-                _locked.Release();
-               // _lock.ReleaseMutex();
-            }
+            
         }
 
-        static void Thread_2()
+        static Reward GetRewardById(int id)
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                _locked.Acquire();
-                _num--;
-                _locked.Release();
-            }
+            _lock.EnterReadLock();
+
+            _lock.ExitReadLock();
+            return null;
         }
 
-        static void Main(string[] args) 
+        static Reward AddReward(int id)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            _lock.EnterWriteLock();
 
-            t1.Start();
-            t2.Start();
+            _lock.ExitWriteLock();
+            return null;
+        }
 
-            Task.WaitAll(t1, t2);
-            Console.WriteLine(_num);
+        static void Main(string[] args)
+        {
+
         }
     }
 }

291e432176e06aed14f277b0f21dd20f0017dce1
LucaSeoyoun <zzzzqqqw@naver.com>
2024-08-15 17:44:49 +0900

    ReaderWriterLock 구현 연습 - 재귀적 락 허용 Yes

--- ServerStudy/Lock.cs
+++ ServerStudy/Lock.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace ServerStudy
 {
-    // 재귀적 락을 허용할지 (No)
+    // 재귀적 락을 허용할지 (Yes)
+    // WriteLock일 때 -> WriteLock OK, WriteLock일 때 -> ReadLock OK
+    // ReadLock일 때엔 이미 모두가 점유 가능하므로 -> WriteLock NO(처리 안 함)
     // 스핀락 정책 (5000번 -> Yield)
 
     class Lock
@@ -17,19 +19,28 @@ namespace ServerStudy
         const int MAX_SPIN_COUNT = 5000;
 
         // [Unused(1)] [WriteThreadId(15)] [ReadCount(16)]
-        int _flag = EMPTY_FLAG; 
+        int _flag = EMPTY_FLAG;
+        int _writeCount = 0;
 
         public void WriteLock()
         {
+            // 동일 스레드가 WriteLock을 이미 획득하고 있는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                _writeCount++;
+                return;
+            }
+
             // 아무도 WriteLock or ReadLock을 획득하고 있지 않을 때, 경합해서 소유권을 얻는다
             int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
             while (true)
             {
                 for (int i = 0; i < MAX_SPIN_COUNT; i++)
                 {
-                    // 시도를 해서 성공하면 return
                     if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
                     {
+                        _writeCount = 1;
                         return;
                     }
                 }
@@ -40,12 +51,25 @@ namespace ServerStudy
 
         public void WriteUnlock()
         {
-            // 초기화
-            Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+            // WriteLock의 카운트와 짝을 맞춤.
+            int lockCount = --_writeCount;
+            if (lockCount == 0)
+            {
+                // 초기화
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+            }
         }
 
         public void ReadLock()
         {
+            // 동일 스레드가 WriteLock을 이미 획득하고 있는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+            }
+
             // 아무도 WriteLock을 획득하고 있지 않을 때, ReadCount를 1 늘린다
             while (true)
             {
@@ -55,7 +79,6 @@ namespace ServerStudy
                     // expected = Write 플래그를 0으로 밀어버린 ReadCount의 값.
                     if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
                         return;
-
                 }
             }
         }
